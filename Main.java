593ef61552bd64e59f39159f4b601db44d54bb20
DESKTOP-H6SJVQ1\Martin <mart963g@stud.kea.dk>
2017-08-24 11:41:42 +0200

    Oh shit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main{
+
+   public static void main(String[] args){
+     System.out.print("Hello, World!");
+   }
+   
+   public static void HelloWorld() {
+   System.out.println(" world!");
+   }
+   
+   public static void HW(String text){
+   System.out.print(text);
+   }
+
+}
