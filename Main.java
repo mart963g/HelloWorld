2e2221ccc11d409619d2a43392c26b113b7bcae6
DESKTOP-H6SJVQ1\Martin <mart963g@stud.kea.dk>
2017-08-24 12:30:04 +0200

    Wubbalubbadubdub

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
 
    public static void main(String[] args){
-     System.out.print("Wut up!");
+     System.out.print("Wubbalubbadubdub!");
    }
    
    public static void HelloWorld() {
